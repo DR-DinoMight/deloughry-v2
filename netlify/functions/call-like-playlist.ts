82dab0487b14071029e3473fb6bdca1de7e92fef
Matthew Peck-Deloughry <matt@deloughry.co.uk>
2023-03-20 22:51:57 +0000

    Spotify playlist

--- netlify/functions/call-like-playlist.ts
+++ netlify/functions/call-like-playlist.ts
@@ -1,4 +1,5 @@
 import { Handler, HandlerEvent, HandlerContext, schedule } from "@netlify/functions";
+import fetch from 'node-fetch';
 
 const myHandler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
     const response = await fetch('https://deloughry.co.uk/api/spotify/liked-tracks-playlist',
