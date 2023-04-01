b1dcec069fa4540ffc2451a6c75f09a2d47ae9cc
Matthew Peck-Deloughry <matt@deloughry.co.uk>
2023-04-01 10:25:09 +0100

    further hacks

---
+++ src/components/ImageWithBadge/index.tsx
@@ -0,0 +1,52 @@
+
+import React, { useState } from 'react';
+import './style.css';
+import { SpotifyLogo } from './spotify-logo';
+import { PlayIcon } from './play-icon';
+
+interface ImageWithBadgeTypes {
+  imageUrl: string;
+  underText?: string;
+}
+
+
+
+const ImageWithBadge = ({ imageUrl, underText }: ImageWithBadgeTypes) => {
+  const [hovered, setHovered] = useState(false);
+  const [svgToDisplay, setSvgToDisplay] = useState("spotify");
+
+  const handleMouseEnter = () => {
+    setHovered(true);
+    // setTimeout(() => {
+    setSvgToDisplay('play');
+    // }, 200);
+  };
+
+  const handleMouseLeave = () => {
+    setHovered(false);
+    setTimeout(() => {
+      setSvgToDisplay('spotify');
+    }, 200);
+  };
+
+  return (
+    <div>
+      <div className={`container ${hovered ? 'active' : ''}`}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}>
+        <div className={`overlay`}>
+          {svgToDisplay == "play" ? <PlayIcon className="play" /> : <SpotifyLogo className="logo" />}
+        </div>
+        <img
+          className="image"
+          src={imageUrl}
+          alt=""
+        />
+      </div>
+
+      {underText}
+    </div>
+  );
+};
+
+export default ImageWithBadge;
--- src/data/models/playlist.ts
+++ src/data/models/playlist.ts
@@ -1,4 +1,7 @@
 import { Field, PrimaryKey, TigrisCollection, TigrisDataTypes } from "@tigrisdata/core";
+import { Artist, Track } from "./tracks";
+import { getAllPlaylists } from "../../lib/spotify";
+import tigrisDB from "../../lib/tigris";
 
 
 @TigrisCollection("playlists")
@@ -21,9 +24,66 @@ export class Playlists {
   @Field(TigrisDataTypes.STRING)
   externalUrl: string;
 
+  @Field(TigrisDataTypes.ARRAY, { elements: Track})
+  tracks?: Array<Track>;
+
   @Field(TigrisDataTypes.DATE_TIME, { timestamp: "createdAt" })
   createdAt?: Date;
 
   @Field(TigrisDataTypes.DATE_TIME, { timestamp: "updatedAt" })
   updatedAt?: Date;
+}
+
+
+export async function addTracksToPlaylistDb(id: string, tracks: any[]) {
+  const dbPlaylists = tigrisDB.getCollection<Playlists>("playlists");
+  const dbTracks = tigrisDB.getCollection<Track>("tracks");
+
+  const playlists = await getAllPlaylists();
+  const playlist = playlists.find(x => x.id === id);
+
+  if (playlist) {
+
+        // insert or update the track to the db
+        const tracksInDB = await dbTracks.insertOrReplaceMany(
+          tracks.map( track => {
+
+            const artists : Artist[] = track.artists.map( artist => {
+              return {
+                id: artist.id,
+                name: artist.name
+              }
+            });
+
+
+            return {
+              id: track.id,
+              artists: artists,
+              name: track.name,
+              artwork: track?.images ?? track.album.images,
+              uri: track?.uri,
+              previewUrl: track?.preview_url ?? '',
+              externalUrl: track?.album?.external_urls['spotify'] ?? '',
+              addedAt: new Date()
+            }
+          })
+        );
+
+        // if ( ! await dbPlaylists.findOne({ filter: { id: playlist.id}})){
+          // console.log("No playlist found with id: " + playlist.id);s
+          await dbPlaylists.insertOrReplaceOne({
+            id: playlist.id,
+            name: playlist.name,
+            description: playlist.description,
+            artwork: playlist.images[0].url,
+            spotifyUri: playlist.uri,
+            externalUrl: playlist.external_urls['spotify'],
+            tracks: tracksInDB,
+            updatedAt: new Date()
+          })
+
+        // }
+
+        return playlist.id;
+      }
 }
