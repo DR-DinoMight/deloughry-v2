410cb708d7f19dc6580fef5c6d779fa849b69a5d
Matthew Peck-Deloughry <matt@deloughry.co.uk>
2023-06-12 11:27:05 +0100

    Added Playlist dpeloy call

--- src/data/models/playlist.ts
+++ src/data/models/playlist.ts
@@ -195,16 +195,6 @@ export async function addTracksToPlaylistDb(
     // tx.commit();
     prisma.$disconnect();
 
-    if (fromAPI) {
-      console.log("building");
-      fetch(
-        `${import.meta.env.BUILD_HOOK}?trigger_title=New+Song+or+playlist+Added&clear_cache=true`,
-        {
-          method: "POST",
-        }
-      );
-    }
-
     return playlist.id;
   }
   catch (e) {
