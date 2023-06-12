263527420108fd3328d63d229afd88ed8dd62218
Matthew Peck-Deloughry <matt@deloughry.co.uk>
2023-06-12 21:42:45 +0100

    wip

---
+++ astro.config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
+
+
+// https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://deloughry.co.uk",
+  markdown: {
+    shikiConfig: {
+      theme: "poimandres",
+      wrap: false,
+    },
+  },
+  output: "server",
+  integrations: [
+    mdx({}),
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    sitemap(),
+    prefetch(),
+    react(),
+  ],
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
+  adapter: netlify(),
+});
