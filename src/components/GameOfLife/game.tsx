08039a94b0aeb82a5a8aad8a89010c1d2588a464
Matthew Peck-Deloughry <matt@deloughry.co.uk>
2023-06-20 20:41:16 +0100

    Added GOL

---
+++ src/components/GameOfLife/game.tsx
@@ -0,0 +1,146 @@
+import React, { useState, useCallback, useRef } from 'react';
+
+const numofRows = 20;
+const numofCols = 20;
+
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0]
+];
+
+// Pulsar pattern in a 20 x 20 grid
+
+const pulsarPattern = [
+  [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+  [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
+  [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0]
+];
+
+const generateEmptyGrid = () => {
+  const rows = [];
+  for (let i = 0; i < numofRows; i++) {
+    rows.push(Array.from(Array(numofCols), () => 0));
+  }
+  return rows;
+}
+
+const GameOfLife = () => {
+  const [grid, setGrid] = useState(() => {
+    const rows = generateEmptyGrid();
+
+    // Insert the pulsar pattern at the center of the grid
+    const startRow = Math.floor(numofRows / 2) - Math.floor(pulsarPattern.length / 2);
+    const startCol = Math.floor(numofCols / 2) - Math.floor(pulsarPattern[0].length / 2);
+
+    for (let i = 0; i < pulsarPattern.length; i++) {
+      for (let j = 0; j < pulsarPattern[i].length; j++) {
+        rows[startRow + i][startCol + j] = pulsarPattern[i][j];
+      }
+    }
+
+    return rows;
+  });
+
+  const [running, setRunning] = useState(false);
+
+  const runningRef = useRef(running);
+  runningRef.current = running;
+
+  const runSimulation = useCallback(() => {
+    if (!runningRef.current) {
+      return;
+    }
+
+    setGrid((oldGrid) => {
+      const newGrid = JSON.parse(JSON.stringify(oldGrid));
+      for (let i = 0; i < numofRows; i++) {
+        for (let j = 0; j < numofCols; j++) {
+          let neighbors = 0;
+          operations.forEach(([x, y]) => {
+            const newI = i + x;
+            const newJ = j + y;
+            if (newI >= 0 && newI < numofRows && newJ >= 0 && newJ < numofCols) {
+              neighbors += oldGrid[newI][newJ];
+            }
+          });
+
+          if (neighbors < 2 || neighbors > 3) {
+            newGrid[i][j] = 0;
+          } else if (oldGrid[i][j] === 0 && neighbors === 3) {
+            newGrid[i][j] = 1;
+          }
+        }
+      }
+      return newGrid;
+    });
+
+    setTimeout(runSimulation, 200);
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h2>React</h2>
+      <div className="flex gap-2 items-center justify-center align-middle">
+        <button
+          class="flex w-full justify-center rounded-md bg-accent px-3 py-1.5 text-sm font-semibold leading-6 text-bgColor shadow-sm hover:bg-accent-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-accent-dark"
+          onClick={() => {
+            setRunning(!running);
+            if (!running) {
+              runningRef.current = true;
+              runSimulation();
+            }
+          }}
+        >
+          {running ? 'Stop' : 'Start'}
+        </button>
+        <button onClick={generateEmptyGrid} class="flex w-full justify-center rounded-md bg-accent px-3 py-1.5 text-sm font-semibold leading-6 text-bgColor shadow-sm hover:bg-accent-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-accent-dark">
+          Clear
+        </button>
+        <button
+          class="flex w-full justify-center rounded-md bg-accent px-3 py-1.5 text-sm font-semibold leading-6 text-bgColor shadow-sm hover:bg-accent-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-accent-dark"
+          onClick={() => {
+            const rows = generateEmptyGrid();
+            for (let i = 0; i < numofRows; i++) {
+              for (let j = 0; j < numofCols; j++) {
+                rows[i][j] = Math.random() > 0.7 ? 1 : 0;
+              }
+            }
+            setGrid(rows);
+          }}>Random</button>
+      </div>
+      <div className="grid grid-cols-20 gap-1 mt-8">
+        {grid.map((rows, i) =>
+          rows.map((col, j) => (
+            <div
+              key={`${i}-${j}`}
+              onClick={() => {
+                const newGrid = [...grid];
+                newGrid[i][j] = grid[i][j] ? 0 : 1;
+                setGrid(newGrid);
+              }}
+              className={`w-6 h-6 rounded-md shadow transition-all duration-300 transform hover:cursor-pointer hover:scale-110 ${grid[i][j] ? 'bg-accent hover:bg-accent-dark' : 'bg-accent-bg hover:brightness-50'}`}
+            />
+          ))
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default GameOfLife;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,126 +3,133 @@ const plugin = require("tailwindcss/plugin");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
-	darkMode: "class",
-	corePlugins: {
-		// disable aspect ratio as per docs -> @tailwindcss/aspect-ratio
-		aspectRatio: false,
-		// disable some core plugins as they are included in the css, even when unused
-		touchAction: false,
-		ringOffsetWidth: false,
-		ringOffsetColor: false,
-		scrollSnapType: false,
-		borderOpacity: false,
-		textOpacity: false,
-		fontVariantNumeric: false,
-	},
-	theme: {
-		listStyleType: {
-			decimal: "decimal",
-			disc: "disc",
-		},
-		extend: {
+  content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  corePlugins: {
+    // disable aspect ratio as per docs -> @tailwindcss/aspect-ratio
+    aspectRatio: false,
+    // disable some core plugins as they are included in the css, even when unused
+    touchAction: false,
+    ringOffsetWidth: false,
+    ringOffsetColor: false,
+    scrollSnapType: false,
+    borderOpacity: false,
+    textOpacity: false,
+    fontVariantNumeric: false,
+  },
+  theme: {
+    listStyleType: {
+      decimal: "decimal",
+      disc: "disc",
+    },
+    extend: {
+      gridTemplateColumns: {
+        '20': 'repeat(20, minmax(0, 1fr))',
+        '50': 'repeat(50, minmax(0, 1fr))',
+      },
       fontSize: {
         '20xl': '8rem',
       },
-			colors: {
-				bgColor: "var(--theme-bg)",
-				textColor: "var(--theme-text)",
-				link: "var(--theme-link)",
-				accent: "var(--theme-accent)",
-				"accent-2": "var(--theme-accent-2)",
-				twitch: "var(--twitch)",
-				twitter: "var(--twitter)",
-				mastodon: "var(--mastodon)",
-				youtube: "var(--youtube)",
-				claw: "var(--claw)",
-				github: "var(--github)",
-			},
-			fontFamily: {
-				// Add any custom fonts here
-				mono: ["JetBrains Mono"],
-				sans: [...fontFamily.sans],
-				serif: [...fontFamily.serif],
-			},
-			transitionProperty: {
-				height: "height",
-			},
-			typography: (theme) => ({
-				cactus: {
-					css: {
-						"--tw-prose-body": "var(--theme-text)",
-						"--tw-prose-headings": "var(--theme-accent-2)",
-						"--tw-prose-links": "var(--theme-text)",
-						"--tw-prose-bold": "var(--theme-text)",
-						"--tw-prose-bullets": "var(--theme-text)",
-						"--tw-prose-quotes": "var(--theme-quote)",
-						"--tw-prose-code": "var(--theme-text)",
-						"--tw-prose-hr": "0.5px dashed #666",
-						"--tw-prose-th-borders": "#666",
-					},
-				},
-				DEFAULT: {
-					css: {
-						a: {
-							"@apply cactus-link text-accent no-underline": "",
-						},
-						strong: {
-							fontWeight: "700",
-						},
-						code: {
-							border: "1px dotted #666",
-							borderRadius: "2px",
-						},
-						blockquote: {
-							borderLeftWidth: "none",
-						},
-						hr: {
-							borderTopStyle: "dashed",
-						},
-						thead: {
-							borderBottomWidth: "none",
-						},
-						"thead th": {
-							fontWeight: "700",
-							borderBottom: "1px dashed #666",
-						},
-						"tbody tr": {
-							borderBottomWidth: "none",
-						},
-						tfoot: {
-							borderTop: "1px dashed #666",
-						},
-					},
-				},
-				sm: {
-					css: {
-						code: {
-							fontSize: theme("fontSize.sm")[0],
-							fontWeight: "400",
-						},
-					},
-				},
-			}),
-		},
-	},
-	plugins: [
-		require("@tailwindcss/typography"),
-		require("@tailwindcss/line-clamp"),
-		require("@tailwindcss/aspect-ratio"),
-		plugin(function ({ addComponents }) {
-			addComponents({
-				".cactus-link": {
-					color: "var(--theme-accent)",
-					"@apply bg-[size:100%_6px] bg-bottom bg-repeat-x no-underline": {},
-					"&:hover": {
-						color: "var(--theme-link)",
-					},
-				},
-				".title": {
-					"@apply text-2xl font-semibold text-accent-2": {},
-				},
-			});
-		}),
-	],
+      colors: {
+        bgColor: "var(--theme-bg)",
+        textColor: "var(--theme-text)",
+        link: "var(--theme-link)",
+        accent: "var(--theme-accent)",
+        "accent-dark": "var(--theme-accent-hover)",
+        "accent-2": "var(--theme-accent-2)",
+        "accent-bg": "var(--theme-accent-bg)",
+        twitch: "var(--twitch)",
+        twitter: "var(--twitter)",
+        mastodon: "var(--mastodon)",
+        youtube: "var(--youtube)",
+        claw: "var(--claw)",
+        github: "var(--github)",
+
+      },
+      fontFamily: {
+        // Add any custom fonts here
+        mono: ["JetBrains Mono"],
+        sans: [...fontFamily.sans],
+        serif: [...fontFamily.serif],
+      },
+      transitionProperty: {
+        height: "height",
+      },
+      typography: (theme) => ({
+        cactus: {
+          css: {
+            "--tw-prose-body": "var(--theme-text)",
+            "--tw-prose-headings": "var(--theme-accent-2)",
+            "--tw-prose-links": "var(--theme-text)",
+            "--tw-prose-bold": "var(--theme-text)",
+            "--tw-prose-bullets": "var(--theme-text)",
+            "--tw-prose-quotes": "var(--theme-quote)",
+            "--tw-prose-code": "var(--theme-text)",
+            "--tw-prose-hr": "0.5px dashed #666",
+            "--tw-prose-th-borders": "#666",
+          },
+        },
+        DEFAULT: {
+          css: {
+            a: {
+              "@apply cactus-link text-accent no-underline": "",
+            },
+            strong: {
+              fontWeight: "700",
+            },
+            code: {
+              border: "1px dotted #666",
+              borderRadius: "2px",
+            },
+            blockquote: {
+              borderLeftWidth: "none",
+            },
+            hr: {
+              borderTopStyle: "dashed",
+            },
+            thead: {
+              borderBottomWidth: "none",
+            },
+            "thead th": {
+              fontWeight: "700",
+              borderBottom: "1px dashed #666",
+            },
+            "tbody tr": {
+              borderBottomWidth: "none",
+            },
+            tfoot: {
+              borderTop: "1px dashed #666",
+            },
+          },
+        },
+        sm: {
+          css: {
+            code: {
+              fontSize: theme("fontSize.sm")[0],
+              fontWeight: "400",
+            },
+          },
+        },
+      }),
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
+    plugin(function ({ addComponents }) {
+      addComponents({
+        ".cactus-link": {
+          color: "var(--theme-accent)",
+          "@apply bg-[size:100%_6px] bg-bottom bg-repeat-x no-underline": {},
+          "&:hover": {
+            color: "var(--theme-link)",
+          },
+        },
+        ".title": {
+          "@apply text-2xl font-semibold text-accent-2": {},
+        },
+      });
+    }),
+  ],
 };
