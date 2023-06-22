42bc877b8c0ae7f971f6fa7d4ac17550294bd85b
Matthew Peck-Deloughry <matt@deloughry.co.uk>
2023-06-22 21:02:21 +0100

    push

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,20 @@
 import { z, defineCollection } from "astro:content";
 
 function removeDupsAndLowerCase(array: string[]) {
-	if (!array.length) return array;
-	const lowercaseItems = array.map((str) => str.toLowerCase());
-	const distinctItems = new Set(lowercaseItems);
-	return Array.from(distinctItems);
+  if (!array.length) return array;
+  const lowercaseItems = array.map((str) => str.toLowerCase());
+  const distinctItems = new Set(lowercaseItems);
+  return Array.from(distinctItems);
 }
 
 const post = defineCollection({
-	schema: z.object({
-		title: z.string(),
-		description: z.string().min(50).max(250),
-		publishDate: z.string().transform((str) => new Date(str)),
-		tags: z.array(z.string()).default([]).transform(removeDupsAndLowerCase),
-		ogImage: z.string().optional(),
-	}),
+  schema: z.object({
+    title: z.string(),
+    description: z.string().min(50).max(250),
+    publishDate: z.string().transform((str: string) => new Date(str)),
+    tags: z.array(z.string()).default([]).transform(removeDupsAndLowerCase),
+    ogImage: z.string().optional(),
+  }),
 });
 
 export const collections = { post };
