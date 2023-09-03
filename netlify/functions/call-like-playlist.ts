7ae495b7bd862ac0ebf65a0a7a1a0855851cc8a3
Matthew Deloughry <matt@deloughry.co.uk>
2023-09-03 12:47:10 +0100

    Refactor function names in call-like-playlist.ts file

--- netlify/functions/call-like-playlist.ts
+++ netlify/functions/call-like-playlist.ts
@@ -1,12 +1,12 @@
-import { Handler, HandlerEvent, HandlerContext, schedule } from "@netlify/functions";
+import { type Handler, type HandlerEvent, type HandlerContext, schedule } from "@netlify/functions";
 import fetch from "node-fetch";
 
-const myHandler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-	const response = await fetch("https://deloughry.co.uk/api/spotify/liked-tracks-playlist", {
-		method: "POST",
-	});
+const myhandler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+  const response = await fetch("https://deloughry.co.uk/api/spotify/liked-tracks-playlist", {
+    method: "POST",
+  });
 };
 
-const handler = schedule("*/5 * * * *", myHandler);
+const handler = schedule("*/5 * * * *", myhandler);
 
 export { handler };
