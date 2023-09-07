c637c9e70e6007a2e5c22845c57006239f2528f3
Matthew Deloughry <matt@deloughry.co.uk>
2023-09-07 17:41:24 +0100

    Update astro.config.ts with optimized dependencies and formatting

--- astro.config.ts
+++ astro.config.ts
@@ -4,37 +4,32 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 
-// https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
-	site: "https://deloughry.co.uk",
-	markdown: {
-		shikiConfig: {
-			theme: "poimandres",
-			wrap: false,
-		},
-	},
-	output: "server",
-	integrations: [
-		mdx({}),
-		tailwind({
-			config: {
-				applyBaseStyles: false,
-			},
-		}),
-		sitemap(),
-		prefetch(),
-		react(),
-	],
-	vite: {
-		optimizeDeps: {
-			exclude: ["@resvg/resvg-js"],
-		},
-	},
-	adapter: netlify(),
+  site: "https://deloughry.co.uk",
+  markdown: {
+    shikiConfig: {
+      theme: "poimandres",
+      wrap: false
+    }
+  },
+  output: "server",
+  integrations: [mdx({}), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), sitemap(), prefetch(), react()],
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"]
+    }
+  },
+  adapter: netlify()
 });
