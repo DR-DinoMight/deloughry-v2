280c85a82a51030c1382e869870dedc6dc64f26a
Matthew Deloughry <matt@deloughry.co.uk>
2023-10-09 21:39:35 +0100

    tweaks

--- src/lib/types/spotify.ts
+++ src/lib/types/spotify.ts
@@ -1,4 +1,4 @@
-import type { Artwork, Playlist } from "@prisma/client"
+
 import { Prisma } from "@prisma/client"
 
 export interface SpotifyTrack extends SpotifyApi.TrackObjectFull {
@@ -36,3 +36,41 @@ const trackWithAllRelationships = Prisma.validator<Prisma.TrackArgs>()({
 });
 
 export type TrackWithAllRelationships = Prisma.TrackGetPayload<typeof trackWithAllRelationships>
+
+export type Playlist = {
+  id: string,
+  name: string,
+  description: string,
+  owner: string,
+  tracks: TrackWithAllRelationships[],
+  artwork: Artwork[]
+}
+
+export type Track = {
+  id: string,
+  playlistId: string,
+  name: string,
+  uri: string,
+  previewUrl: string,
+  externalUrl: string,
+  addedAt: string,
+  createdAt: string,
+  updatedAt: string,
+  artists: Artist[],
+  artwork: Artwork[]
+}
+
+
+export type Artist = {
+  id: string,
+  name: string
+}
+
+export type Artwork = {
+  id: string,
+  url: string,
+  height: number,
+  width: number
+  trackId: string,
+  playlistId: string | null
+}
