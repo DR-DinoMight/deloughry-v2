504801e8ee40b20108358b639cbcc3c98d03c273
Matthew Peck-Deloughry <matt@deloughry.co.uk>
2023-12-24 11:46:42 +0000

    new lick of paint

--- astro.config.ts
+++ astro.config.ts
@@ -3,13 +3,14 @@ import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
-
 import react from "@astrojs/react";
 
-// https://astro.build/config
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
+
+// https://astro.build/config
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
@@ -25,7 +26,7 @@ export default defineConfig({
     config: {
       applyBaseStyles: false
     }
-  }), sitemap(), prefetch(), react()],
+  }), sitemap(), prefetch(), react(), compress()],
   vite: {
     optimizeDeps: {
       exclude: ["@resvg/resvg-js"]
@@ -33,6 +34,6 @@ export default defineConfig({
   },
   adapter: netlify({
     // builders: true,
-    edgeMiddleware: true,
+    edgeMiddleware: true
   })
 });
--- src/components/NowPlaying.jsx
+++ src/components/NowPlaying.jsx
@@ -6,78 +6,77 @@ import { ImageWithHeart } from "./ImageWithHeart";
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function NowPlaying() {
-	let interval = null;
-	const [progress, setProgress] = useState(0);
+  let interval = null;
+  const [progress, setProgress] = useState(0);
 
   // const { data, mutate, error, isLoading } = useSWR("http://localhost:3000/spotify/now-playing", fetcher, {
-	// 	onSuccess: (data) => {
-	// 		clearInterval(interval);
-	// 		setProgress(data?.song?.progress);
-	// 	},
-	// });
+  // 	onSuccess: (data) => {
+  // 		clearInterval(interval);
+  // 		setProgress(data?.song?.progress);
+  // 	},
+  // });
 
-	const { data, mutate, error, isLoading } = useSWR("https://api.deloughry.co.uk/spotify/now-playing", fetcher, {
-		onSuccess: (data) => {
-			clearInterval(interval);
-			setProgress(data?.song?.progress);
-		},
-	});
+  const { data, mutate, error, isLoading } = useSWR("https://api.deloughry.co.uk/spotify/now-playing", fetcher, {
+    onSuccess: (data) => {
+      clearInterval(interval);
+      setProgress(data?.song?.progress);
+    },
+  });
 
-	useEffect(() => {
-		if (data) {
-			interval = setInterval(() => {
-				setProgress((prev) => {
-					if (prev >= data?.song?.duration) {
-						clearInterval(interval);
-						mutate();
-					}
-					return prev + 1000;
-				});
-			}, 1000);
-		}
-		return () => clearInterval(interval);
-	}, [data?.song?.progress, data?.song?.duration]);
+  useEffect(() => {
+    if (data) {
+      interval = setInterval(() => {
+        setProgress((prev) => {
+          if (prev >= data?.song?.duration) {
+            clearInterval(interval);
+            mutate();
+          }
+          return prev + 1000;
+        });
+      }, 1000);
+    }
+    return () => clearInterval(interval);
+  }, [data?.song?.progress, data?.song?.duration]);
 
-	return (
-		<div
-			className={`${
-				data?.isPlaying ? "visible" : "invisible"
-			} flex flex-col justify-center align-middle transition-all duration-250 ease-in-out`}
-		>
-			{data?.isPlaying && (
-				<div className="space-around mb-4 flex w-full items-center space-x-0 overflow-hidden rounded-lg bg-zinc-200 p-2 align-middle shadow-lg dark:bg-zinc-700 sm:space-x-2">
-					<ImageWithHeart
-						image={data?.song?.albumArt[2].url}
-						className="relative mr-2 h-[70px]"
-						isLiked={data?.song?.isLiked}
-					/>
-					<div>
-						<span className="flex-col items-center sm:flex-row">
-							{data?.isPlaying ? (
-								<a
-									className="capsize font-sm text-wrap flex w-full flex-col flex-wrap truncate break-all dark:text-white"
-									href={data?.song?.uri}
-									target="_blank"
-									rel="noopener noreferrer"
-								>
-									<span className="truncaste">{data?.song?.name}</span>
-									<span className="capsize truncate break-all text-xs text-zinc-700 dark:text-gray-200">
-										{data?.song?.artist}
-									</span>
-								</a>
-							) : (
-								<p className="capsize font-md text-gray-200">Not Playing</p>
-							)}
-						</span>
+  return (
+    <div
+      className={`${data?.isPlaying ? "visible" : "invisible"
+        } flex flex-col justify-center align-middle transition-all duration-250 ease-in-out`}
+    >
+      {data?.isPlaying && (
+        <div className="sticky space-around mb-4 flex w-full items-center space-x-0 overflow-hidden rounded-lg bg-zinc-200 p-2 align-middle shadow-lg dark:bg-zinc-700 sm:space-x-2">
+          <ImageWithHeart
+            image={data?.song?.albumArt[2].url}
+            className="relative mr-2 h-[70px]"
+            isLiked={data?.song?.isLiked}
+          />
+          <div>
+            <span className="flex-col items-center sm:flex-row">
+              {data?.isPlaying ? (
+                <a
+                  className="capsize font-sm text-wrap flex w-full flex-col flex-wrap truncate break-all dark:text-white no-underline"
+                  href={data?.song?.uri}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <span className="truncaste text-accent font-oswald font-bold text-lg uupercase">{data?.song?.name}</span>
+                  <span className="capsize truncate break-all text-xs text-zinc-700 dark:text-gray-200">
+                    {data?.song?.artist}
+                  </span>
+                </a>
+              ) : (
+                <p className="capsize font-md text-gray-200">Not Playing</p>
+              )}
+            </span>
 
-						<SoundWave
-							progress={Math.floor((progress / data?.song?.duration) * 100) || 0}
-							isPlaying={data?.isPlaying}
-						/>
-					</div>
-				</div>
-			)}
-			{!data?.isPlaying && <p className="min-h-[102px]">Spotify Not Playing</p>}
-		</div>
-	);
+            <SoundWave
+              progress={Math.floor((progress / data?.song?.duration) * 100) || 0}
+              isPlaying={data?.isPlaying}
+            />
+          </div>
+        </div>
+      )}
+      {!data?.isPlaying && <p className="min-h-[102px]">Spotify Not Playing</p>}
+    </div>
+  );
 }
