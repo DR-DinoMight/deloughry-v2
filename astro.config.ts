d47b8586b04e3a13fd000216ae54d3ca60c1f391
Matt Deloughry <matt@deloughry.co.uk>
2024-04-21 20:30:10 +0100

    Added Speed insights

--- astro.config.ts
+++ astro.config.ts
@@ -4,13 +4,14 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 import react from "@astrojs/react";
-
 import netlify from "@astrojs/netlify/functions";
 
-// https://astro.build/config
 
 // https://astro.build/config
 import compress from "astro-compress";
+
+// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -22,9 +23,6 @@ export default defineConfig({
     }
   },
   output: "server",
-  adapter: netlify({
-    imageCDN: false,
-  }),
   integrations: [mdx({}), tailwind({
     config: {
       applyBaseStyles: false
@@ -35,8 +33,5 @@ export default defineConfig({
       exclude: ["@resvg/resvg-js"]
     }
   },
-  adapter: netlify({
-    // builders: true,
-    edgeMiddleware: true
-  })
+  adapter: vercel()
 });
