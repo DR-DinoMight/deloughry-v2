4b99eb181de28bdf12903ef7020270e6b52be3b2
Matthew Deloughry <matt@deloughry.co.uk>
2024-04-23 18:29:39 +0100

    tweaks

--- src/lib/ogFetchers.ts
+++ src/lib/ogFetchers.ts
@@ -2,8 +2,13 @@ import ogs from 'open-graph-scraper';
 
 const ogFetcher = async (url: string) => {
     const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36';
-    const data = await ogs({url: url, timeout: 5000, onlyGetOpenGraphInfo: true, fetchOptions: { headers: { 'user-agent': userAgent } }});
-    return data?.result ?? []
+    try {
+      const data = await ogs({url: url, timeout: 5000, onlyGetOpenGraphInfo: true, fetchOptions: { headers: { 'user-agent': userAgent } }});
+      return data?.result ?? []
+    } catch (error) {
+      console.error(error);
+    }
+
 }
 
 export {
