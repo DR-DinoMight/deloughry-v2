fa80d0c239ab43928cc6832dba85adb8bddd1886
Matthew Deloughry <matt@deloughry.co.uk>
2024-04-28 22:18:09 +0100

    tweaks

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/rss_B5HbKWta.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/rss_Duskai3z.mjs').then(n => n.r);
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/work-in-progress_wo5-VtXY.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./work-in-progress_BF1fGOfv.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
