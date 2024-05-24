c96c26de3026de0b632448109509c6ccb52b890d
Matthew Deloughry <matt@deloughry.co.uk>
2024-05-24 13:17:56 +0100

    removed preview button

--- src/components/Tracks.tsx
+++ src/components/Tracks.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
-import AudioPlayer from "./AudioPlayer";
+
 import type { TrackWithAllRelationships } from "@/lib/types/spotify";
 
 
@@ -10,65 +9,20 @@ type TracksProps = {
 const Tracks: React.FC<TracksProps> = ({
   tracks
 }) => {
-  const [currentPlayingTrack, setcurrentPlayingTrack] = useState<string | null>(
-    null
-  );
-  const audioRef = useRef<HTMLAudioElement | null>(null);
 
-  const handlePlay = (src: string) => {
-    if (currentPlayingTrack !== null) {
-      const currentAudio = audioRef.current;
-      if (currentAudio !== null) {
-        currentAudio.pause();
-        currentAudio.currentTime = 0;
-      }
-    }
-    const newAudio = new Audio(src);
-    newAudio.play();
-    setcurrentPlayingTrack(src);
-    audioRef.current = newAudio;
-  };
-
-  const handleStop = () => {
-    if (currentPlayingTrack !== null) {
-      const currentAudio = audioRef.current;
-      if (currentAudio !== null) {
-        currentAudio.pause();
-        currentAudio.currentTime = 0;
-      }
-      setcurrentPlayingTrack(null);
-    }
-  };
-
-  useEffect(() => {
-    const currentAudio = audioRef.current;
-    if (currentAudio !== null) {
-      currentAudio.addEventListener("ended", handleStop);
-      return () => {
-        currentAudio.removeEventListener("ended", handleStop);
-      };
-    }
-  }, [currentPlayingTrack, handleStop]);
   return (
     <ol>
       {
         tracks.flatMap((track, index) => (
           <li className="flex flex-row justify-between" key={track.id}>
-            <a className={`flex flex-row gap-1 flex-wrap mb-2 align-middle ${currentPlayingTrack === track.previewUrl ? 'text-link' : 'cactus-link'} no-underline`} href={track.externalUrl} title="View Track in Spotify" target="_BLANK">
+            <a className={`flex flex-row gap-1 flex-wrap mb-2 align-middle cactus-link no-underline`} href={track.externalUrl} title="View Track in Spotify" target="_BLANK">
               <span>{index + 1}.</span>
               <div className="flex flex-col flex-wrap flex-1">
                 <span>{track.name}</span>
                 <span className="text-xs text-textColor"> {track.artists.flatMap(artist => artist.name).join(', ')}</span>
               </div>
             </a>
-            {track.previewUrl && (
-              < AudioPlayer
-                key={track.id}
-                src={track.previewUrl}
-                isPlaying={currentPlayingTrack === track.previewUrl}
-                onPlay={() => handlePlay(track.previewUrl ?? "")}
-                onStop={handleStop} />
-            )}
+
           </li>
         ))
       }
